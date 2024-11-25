7c0bd076bd5145917c6894623ac6bc6050d9fde4
Leon Brimacombe <olibrimacombe@gmail.com>
2024-11-25 16:58:52 +0000

    create crud actions and views for MVC

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,27 @@
 class RecommendationsController < ApplicationController
+  def New
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    @property.save!
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(
+      :street_address,
+      :postcode,
+      :description,
+      :bedrooms,
+      :bathrooms,
+      :garden,
+      :image_urls,
+      :council_tax,
+      :type,
+      :floor_area,
+    )
+  end
 end
