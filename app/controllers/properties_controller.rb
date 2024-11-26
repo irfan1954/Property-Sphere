e9bda3c4a458c4bad5a3fe454ceacb6967c11df0
aderemi <73750355+gabrieladeremi@users.noreply.github.com>
2024-11-26 14:47:35 +0000

    implementation of show, index and home actions for property controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,14 +1,19 @@
 class PropertiesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home index show]
 
+  before_action :set_property, only: %i[show]
+
   def home
+    @properties = Property.order("RANDOM()").take(10)
   end
 
   def index
-    @properties = Property.all
+    @properties = Property.all.order(:created_at).reverse_order
   end
 
-  def show
+  private
+
+  def set_property
     @property = Property.find(params[:id])
   end
 end
