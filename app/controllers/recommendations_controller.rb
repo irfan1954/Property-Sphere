91106d3158be584485ea47fdc6c63d2abc89ef54
irfan akcay <irfan.akcay@gmail.com>
2024-12-02 14:43:45 +0000

    added creating and deleting option on recommendation form

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,25 +3,40 @@ class RecommendationsController < ApplicationController
     @recommendation = Recommendation.new
   end
 
+  def index
+    @recommendations = Recommendation.all
+  end
+
   def create
     @recommendation = Recommendation.new(recommendation_params)
     @recommendation.user_id = current_user.id
-    rec_postcode = @recommendation.location.postcode.split.strip.join.upcase
+    # rec_postcode = @recommendation.location.postcode.split.strip.join.upcase
     # pcds transformation in database
-    data_postcode = Location.find(pcds: rec_postcode)
-    @recommendation.location_id = data_postcode.id
-    if @recommendation.save!
-      redirect_to @recommendation, notice: 'Recommendation was successfully created.'
+    # data_postcode = Location.find(pcds: rec_postcode)
+    # @recommendation.location_id = data_postcode.id
+    if @recommendation.save
+      redirect_to recommendations_path, notice: 'Recommendation was successfully created.'
     else
       render :new
     end
   end
 
+  def show
+    @recommendation = Recommendation.find(recommendation_params[:location_id])
+  end
+
+  def destroy
+    @recommendation = Recommendation.find(params[:id])
+    @recommendation.destroy
+    redirect_to recommendations_path, notice: "Recommendation deleted."
+  end
+
+
   private
 
   def recommendation_params
     params.require(:recommendation).permit(
-      :comment
+      :comment, :location_id
     )
   end
 end
