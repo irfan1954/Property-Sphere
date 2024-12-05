6270902c0a870f97d2b2962efa1067bc04dd4c02
TobyBromet <tobybromet@gmail.com>
2024-12-05 11:18:21 +0000

    Harmonised buttons. Fixed contact form to update status on

--- app/controllers/saved_properties_controller.rb
+++ app/controllers/saved_properties_controller.rb
@@ -4,40 +4,29 @@ class SavedPropertiesController < ApplicationController
   end
 
   def create
-    @saved_property = SavedProperty.new
-    @saved_property.user_id = current_user.id
     @property = Property.find(params[:property_id])
-    @saved_property.property = @property
-    if @saved_property.save
-      flash[:notice] = 'Added to wishlist'
-      redirect_to request.referrer
-    else
-      flash[:alert] = 'Already in your wishlist'
-      redirect_to request.referrer
-    end
-  end
-
-  def agent_create
-    @property = Property.find(params[:property_id])
-    # Check if the property already exists for the current user
     @saved_property = SavedProperty.find_or_initialize_by(user_id: current_user.id, property_id: @property.id)
 
     if @saved_property.new_record?
       # If it's a new record, assign the additional attributes and save
-      @saved_property.assign_attributes(saved_properties_param)
+      @saved_property.contacted = true
       if @saved_property.save
-        flash[:notice] = 'Added to wishlist'
+        flash[:notice] = 'Sent and added to wishlist'
+        redirect_to request.referrer
       else
         flash[:alert] = 'There was an error saving the property'
+        redirect_to request.referrer
         return
       end
     else
       # If the record exists, update the `contacted` status
       @saved_property.contacted = true
       @saved_property.save
+      redirect_to request.referrer
+      flash[:notice] = 'Message sent'
     end
     # AgentMailer.with(user: current_user).send_email(@saved_property, saved_properties_params[:message])
-    format.html { redirect_to saved_properties_path, notice: "Email was sent successfully" }
+    # format.html { redirect_to saved_properties_path, notice: "Email was sent successfully" }
   end
 
   def update
@@ -62,7 +51,7 @@ class SavedPropertiesController < ApplicationController
   private
 
   def saved_properties_param
-    params.require(:saved_property).permit(:property_id, :message, :comment)
+    params.require(:saved_property).permit(:property_id)
   end
 
   def comment_param
